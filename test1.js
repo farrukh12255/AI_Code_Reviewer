9bff09e58020e548925ef649c45e80e2c9c9e1c9
farrukh12255 <farrukhferoz12@gmail.com>
2025-10-31 00:20:54 +0500

    updated

--- test1.js
+++ test1.js
@@ -1,202 +1,103 @@
-// 100 lines of gloriously messy JS
-var x = 0,
-  y = 10,
-  z = "hello";
-function a() {
-  return Math.random() * 100;
-}
-function b(q) {
-  return q * q;
-}
-var c = [1, 2, 3, 4, 5];
-var d = { foo: "bar", baz: 42 };
-function e(f) {
-  for (var i = 0; i < 10; i++) {
-    f += i;
-  }
-  return f;
-}
-x += a();
-y += b(5);
-console.log("x:", x, "y:", y);
-var foo = "world";
-function f(g) {
-  if (g > 5) {
-    return g * 2;
-  } else {
-    return g / 2;
-  }
-}
-for (var i = 0; i < 5; i++) {
-  console.log(i, f(i));
-}
-console.log(d.foo, d.baz);
+// ====================================================
+// Perfected version of your JS example
+// ====================================================
 
-function g(h) {
-  return h.split("").reverse().join("");
-}
-console.log(g("javascript"));
-var arr = ["a", "b", "c", "d"];
-arr.forEach(function (v, i) {
-  console.log(i, v);
-});
-var sum = 0;
-for (var i = 0; i < 10; i++) {
-  sum += i;
-}
-console.log("sum:", sum);
-function h(j) {
-  return j % 2 == 0 ? "even" : "odd";
-}
-for (var k = 0; k < 10; k++) {
-  console.log(k, h(k));
-}
+var a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var b = [];
 
-var str = "messy code";
-function i(l) {
-  return l.toUpperCase();
-}
-console.log(i(str));
-var m = { x: 1, y: 2, z: 3 };
-for (var key in m) {
-  console.log(key, m[key]);
-}
-var n = [10, 20, 30];
-var total = n.reduce(function (p, q) {
-  return p + q;
-}, 0);
-console.log("total:", total);
+console.log("Starting code...");
+debugger;
 
-function j(o) {
-  while (o < 5) {
-    console.log("loop", o);
-    o++;
-  }
-  return o;
-}
-console.log(j(0));
-var p = [1, 2, 3, 4, 5];
-p.map(function (r) {
-  return r * 2;
-}).forEach(function (s) {
-  console.log(s);
-});
-var q = "concat";
-var r = q + "enation";
-console.log(r);
-
-function k(t) {
-  try {
-    if (t < 0) {
-      throw "negative number";
+// --- Asynchronous Task (simulate heavy async work) ---
+setTimeout(function () {
+  console.log("Async call started...");
+  var count = 0;
+  var limit = 100000;
+  for (var i = 0; i < limit; i++) {
+    for (var j = 0; j < limit; j++) {
+      if ((i + j) % 25000 === 0 && count < 10) {
+        console.log("Still running...", i, j);
+        count++;
+      }
     }
-  } catch (e) {
-    console.log("error:", e);
-  } finally {
-    console.log("finally block");
   }
+  console.log("Async loop finished.");
+}, 1000);
+
+// --- Bubble Sort Implementation (optimized) ---
+function sortArray(arr) {
+  console.log("Sorting started...");
+  var n = arr.length;
+  var swapped;
+  do {
+    swapped = false;
+    for (var i = 0; i < n - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        var temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        swapped = true;
+      }
+    }
+  } while (swapped);
+  console.log("Sorting completed.");
+  return arr;
 }
-k(-1);
-var u = ["x", "y", "z"];
-for (var v of u) {
-  console.log(v);
-}
-var w = [{ a: 1 }, { b: 2 }];
-w.forEach(function (obj) {
-  for (var key in obj) {
-    console.log(key, obj[key]);
+
+// --- Calculate sum of all pair products ---
+function arraySum(arr) {
+  debugger;
+  console.log("Calculating total sum...");
+  var sum = 0;
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = 0; j < arr.length; j++) {
+      sum += arr[i] * arr[j];
+    }
   }
-});
+  console.log("Sum calculation completed.");
+  return sum;
+}
 
-var aa = 0;
-function l(bb) {
-  aa += bb;
-  return aa;
+// --- Random utility function ---
+function randomFunction(x) {
+  console.log("Random function called for:", x);
+  for (var i = 0; i < 1000; i++) {
+    if (i % 100 === 0) {
+      console.log("Checkpoint:", i);
+    }
+  }
+  debugger;
+  return Math.pow(x, 3);
 }
-console.log(l(5));
-console.log(l(10));
-var cc = "repeat";
-for (var dd = 0; dd < 3; dd++) {
-  console.log(cc + dd);
-}
-function m(ee) {
-  return ee * ee * ee;
-}
-console.log(m(3));
 
-var ff = ["one", "two", "three"];
-ff.forEach(function (gg, hh) {
-  console.log(hh, gg);
-});
-function n(ii) {
-  return ii
-    .split("")
-    .map(function (c) {
-      return c.charCodeAt(0);
-    })
-    .join("-");
+// --- Main Execution Flow ---
+for (var i = 0; i < a.length; i++) {
+  var val = randomFunction(a[i]);
+  b.push(val);
+  console.log("Pushed:", val);
 }
-console.log(n("abc"));
-var jj = { k: 1, l: 2 };
-console.log(Object.keys(jj), Object.values(jj));
 
-function o(kk) {
-  return kk % 3 == 0 ? "div3" : "notdiv3";
-}
-for (var ll = 0; ll < 10; ll++) {
-  console.log(ll, o(ll));
-}
-var mm = [5, 10, 15, 20];
-console.log(
-  mm.filter(function (nn) {
-    return nn > 10;
-  })
-);
-var oo = "weird";
-console.log(oo.repeat(3));
+console.log("Now sorting b...");
+var sortedB = sortArray(b);
 
-function p(pp) {
-  return Math.sqrt(pp);
-}
-console.log(p(16));
-var qq = [1, 2, 3, 4, 5];
-console.log(
-  qq.find(function (rr) {
-    return rr > 3;
-  })
-);
-var ss = { a: "A", b: "B" };
-console.log(ss["a"]);
-var tt = "abc";
-console.log(tt.includes("b"));
+console.log("Now calculating total...");
+var total = arraySum(sortedB);
 
-function q(uu) {
-  return uu + 100;
-}
-console.log(q(50));
-var vv = ["x", "y", "z"];
-console.log(vv.indexOf("y"));
-var ww = 0;
-while (ww < 3) {
-  console.log("while", ww);
-  ww++;
-}
-var xx = "concat" + "enate";
-console.log(xx);
+console.log("Total is:", total);
 
-function r(yy) {
-  return yy.toLowerCase();
-}
-console.log(r("HELLO"));
-var zz = ["red", "green", "blue"];
-for (var aaa of zz) {
-  console.log(aaa);
-}
-var bbb = { c: 3, d: 4 };
-for (var key in bbb) {
-  console.log(key, bbb[key]);
-}
-function s(ccc) {
-  return ccc * 10;
-}
-console.log(s(7));
+// --- Another async task simulation ---
+setTimeout(function () {
+  console.log("Async call #2 started...");
+  var count = 0;
+  for (var i = 0; i < 50000; i++) {
+    for (var j = 0; j < 50000; j++) {
+      if ((i + j) % 30000 === 0 && count < 10) {
+        console.log("Still running async #2...", i, j);
+        count++;
+      }
+    }
+  }
+  console.log("Async #2 complete.");
+}, 2000);
+
+console.log("End of code execution.");
