1de113ef88a87e6d4d8a36dbbdc0973ccf4eb63b
farrukh12255 <farrukhferoz12@gmail.com>
2025-11-04 17:43:43 +0500

    pushed java correct code

--- myProgram.java
+++ myProgram.java
@@ -1,9 +1,9 @@
-// Bad Java code example
-public class badCode {
-public static void main(String args[]){
-System.out.println("Hello world") // missing semicolon
-int x = "abc"; // wrong data type
-if(x = 5) // assignment instead of comparison
-System.out.println("X is 5")
-}
+public class BadCode {
+    public static void main(String[] args) {
+        System.out.println("Hello world"); // Added semicolon
+        int x = 5; // Correct data type
+        if (x == 5) { // Correct comparison
+            System.out.println("X is 5");
+        }
+    }
 }
