4bace8bb6840296ca405e1ec6eb8c3de6a39b43a
farrukh12255 <farrukhferoz12@gmail.com>
2025-11-04 17:23:52 +0500

    java code implmented

---
+++ myProgram.java
@@ -0,0 +1,9 @@
+// Bad Java code example
+public class badCode {
+public static void main(String args[]){
+System.out.println("Hello world") // missing semicolon
+int x = "abc"; // wrong data type
+if(x = 5) // assignment instead of comparison
+System.out.println("X is 5")
+}
+}
